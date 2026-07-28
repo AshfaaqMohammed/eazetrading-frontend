55fe47f5ac1e4b6b3a4f5a9597419487d7ddc39f
AshfaaqMohammed <muzammil.ashfaaq@gmail.com>
2026-07-28 22:51:40 +0530

    completed till 1.47

--- src/page/Home/Home.jsx
+++ src/page/Home/Home.jsx
@@ -3,7 +3,7 @@ import React, { useState } from 'react'
 import AssetTable from './AssetTable';
 import StockChart from './StockChart';
 import { Avatar, AvatarImage } from '@/components/ui/avatar';
-import { DotIcon } from '@radix-ui/react-icons';
+import { Cross1Icon, DotIcon } from '@radix-ui/react-icons';
 import { MessageCircle } from 'lucide-react';
 
 const Home = () => {
@@ -14,65 +14,96 @@ const Home = () => {
         setCategory(value);
     }
 
-  return (
-    <div className='relative'>
-        <div className='lg:flex'>
+    return (
+        <div className='relative'>
+            <div className='lg:flex'>
 
-            <div className='lg:w-[50%] lg:border-r'>
+                <div className='lg:w-[50%] lg:border-r'>
 
-                <div className='p-3 flex items-center gap-4'>
+                    <div className='p-3 flex items-center gap-4'>
 
-                    <Button onClick={() => handleCategory("all")} variant={category == "all" ? "default" : "outline"} className='rounded-full'>All</Button>
-                    <Button onClick={() => handleCategory("top50")} variant={category == "top50" ? "default" : "outline"} className='rounded-full'>Top 50</Button>
-                    <Button onClick={() => handleCategory("topGainers")} variant={category == "topGainers" ? "default" : "outline"} className='rounded-full'>Top Gainers</Button>
-                    <Button onClick={() => handleCategory("topLoosers")} variant={category == "topLoosers" ? "default" : "outline"} className='rounded-full'>Top Lossers</Button>
+                        <Button onClick={() => handleCategory("all")} variant={category == "all" ? "default" : "outline"} className='rounded-full'>All</Button>
+                        <Button onClick={() => handleCategory("top50")} variant={category == "top50" ? "default" : "outline"} className='rounded-full'>Top 50</Button>
+                        <Button onClick={() => handleCategory("topGainers")} variant={category == "topGainers" ? "default" : "outline"} className='rounded-full'>Top Gainers</Button>
+                        <Button onClick={() => handleCategory("topLoosers")} variant={category == "topLoosers" ? "default" : "outline"} className='rounded-full'>Top Lossers</Button>
+
+                    </div>
+                    <AssetTable>
+
+                    </AssetTable>
 
                 </div>
-                <AssetTable>
 
-                </AssetTable>
+                <div className='hidden lg:block lg:w-[50%] p-5'>
+                    <StockChart></StockChart>
 
-            </div>
+                    <div className='flex gap-5 items-center'>
+                        <div>
+                            <Avatar>
+                                <AvatarImage src={"https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400"}></AvatarImage>
+                            </Avatar>
+                        </div>
+                        <div>
+                            <div className='flex items-center gap-2'>
+                                <p>BTC</p>
+                                <DotIcon className='text-gray-400'></DotIcon>
+                                <p className='text-gray-400'>Bitcoin</p>
+                            </div>
+                            <div className='flex items-end gap-2'>
+                                <p className='text-xl font-bold'>
+                                    $69249
+                                </p>
+                                <p className='text-red-600'>
+                                    <span>-1342342.567</span>
+                                    <span>(-0.23423%)</span>
+                                </p>
+                            </div>
+                        </div>
 
-            <div className='hidden lg:block lg:w-[50%] p-5'>
-                <StockChart></StockChart>
-
-                <div className='flex gap-5 items-center'>
-                    <div>
-                        <Avatar>
-                            <AvatarImage src={"https://coin-images.coingecko.com/coins/images/1/large/bitcoin.png?1696501400"}></AvatarImage>
-                        </Avatar>
                     </div>
-                    <div>
-                        <div className='flex items-center gap-2'>
-                        <p>BTC</p>
-                        <DotIcon className='text-gray-400'></DotIcon>
-                        <p className='text-gray-400'>Bitcoin</p>
-                    </div>
-                    <div className='flex items-end gap-2'>
-                        <p className='text-xl font-bold'>
-                            $69249
-                        </p>
-                        <p className='text-red-600'>
-                            <span>-1342342.567</span>
-                            <span>(-0.23423%)</span>
-                        </p>
-                    </div>
-                    </div>
-
                 </div>
             </div>
+            <section className='absolute bottom-5 right-5 z-40 flex flex-col justify-end items-end gap-2'>
+
+                <div className='rounded-md w-[20rem] md:w-[25rem] lg:w-[25rem] h-[70vh] bg-slate-900'>
+                    <div className="flex justify-between items-center border-b px-6 h-[12%]">
+                        <p>Chat Bot</p>
+                        <Button variant='ghost' size="icon">
+                            <Cross1Icon />
+                        </Button>
+                    </div>
+                    <div className='h-[76%] flex flex-col overflow-y-auto gap-5 px-5 py-2 scroll-container'>
+                        <div className="self-start pb-5 w-auto">
+                            <div className="justify-end self-end px-5 py-2 rounded-md bg-slate-700 w-auto">
+                                <p>Hi, Ahad,</p>
+                                <p>you can ask crypto related any questions</p>
+                                <p>like, price, market cap extr.....</p>
+                            </div>
+                        </div>
+
+                        {[1, 1, 1, 1].map((item, i) => (
+                            <div key={i} className={`${i % 2 == 0 ? "self-start" : "self-end"} "pb-5 w-auto"`}>
+
+                                {i % 2 == 0 ? <div className="justify-end self-end px-5 py-2 rounded-md bg-slate-700 w-auto">
+                                    <p>Prompt who are you</p>
+                                </div> : <div className="justify-end self-end px-5 py-2 rounded-md bg-slate-700 w-auto">
+                                    <p>ans Hi, Ahad!</p>
+                                </div>}
+                            </div>
+                        ))}
+
+                    </div>
+                </div>
+
+                <div className='relative w-[10rem] cursor-pointer group'>
+                    <Button className="w-full h-[2rem] gap-2 items-center">
+                        <MessageCircle size={30} className='fill-[#1e293b] -rotate-90 stroke-none group-hover:file-[#1a1a1a]'></MessageCircle>
+                        <span className='text-1xl'>Chat Bot</span>
+                    </Button>
+                </div>
+            </section>
         </div>
-        <section className='absolute bottom-5 right-5 z-40 flex flex-col justify-end items-end gap-2'>
-            <div className='relative w-[10rem] cursor-pointer group'>
-                <Button className="w-full h-[2rem] gap-2 items-center">
-                    <MessageCircle size={30} className='fill-[#1e293b] -rotate-90 stroke-none group-hover:file-[#1a1a1a]'></MessageCircle>
-                    <span className='text-1xl'>Chat Bot</span>
-                </Button>
-            </div>
-        </section>
-    </div>
-  )
+    )
 }
 
 export default Home
