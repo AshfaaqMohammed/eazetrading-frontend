67324b27f1fb554f4c1fd0e50de4d3af42985889
AshfaaqMohammed <ashfaaq.mohammed@infor.com>
2026-07-31 00:33:08 +0530

    completed til 2.57, add money form is done.

---
+++ src/page/wallet/TopupForm.jsx
@@ -0,0 +1,60 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import { Label } from '@/components/ui/label'
+import { RadioGroup, RadioGroupItem } from '@/components/ui/radio-group'
+import { DotFilledIcon } from '@radix-ui/react-icons'
+import React, { useState } from 'react'
+
+const TopupForm = () => {
+    const [amount, setAmount] = useState('')
+    const [paymentMethod, setPaymentMethod] = useState('RAZORPAY')
+
+    const handlePaymentMethodChange = (value) => {
+        setPaymentMethod(value)
+    }
+
+    const handleChange = (e) => {
+        setAmount(e.target.value)
+    }
+
+    const handleSubmit = () => {
+        console.log(amount, paymentMethod);
+    }
+
+  return (
+    <div className='pt-10 space-y-5'>
+        <div>
+            <h1 className='pb-1'>Enter Amount</h1>
+            <Input onChange={handleChange} value={amount} className="py-7 text-lg" placeholder="$9999"></Input>
+        </div>
+
+        <div>
+            <h1 className='pb-2'>Select payment method</h1>
+            <RadioGroup onValueChange={(value)=>handlePaymentMethodChange(value)} className="flex gap-3" defaultValue="RAZORPAY">
+                <div className='flex items-center space-x-2 border p-3 rounded-md cursor-pointer'>
+                    <RadioGroupItem icon={DotFilledIcon} className="h-5 w-5" value="RAZORPAY" id="r1"></RadioGroupItem>
+                    <Label htmlFor="r1">
+                        <div className='bg-white rounded-md px-3 py-2 w-24'>
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/8/89/Razorpay_logo.svg/1200px-Razorpay_logo.svg.png" alt="Razorpay" />
+                        </div>
+                    </Label>
+                </div>
+
+                <div className='flex items-center space-x-2 border p-3 rounded-md cursor-pointer'>
+                    <RadioGroupItem icon={DotFilledIcon} className="h-5 w-5" value="STRIPE" id="s1"></RadioGroupItem>
+                    <Label htmlFor="s1">
+                        <div className='bg-white rounded-md px-3 py-2 w-24'>
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/ba/Stripe_Logo%2C_revised_2016.svg/1200px-Stripe_Logo%2C_revised_2016.svg.png" alt="Stripe" />
+                        </div>
+                    </Label>
+                </div>
+            </RadioGroup>
+        </div>
+        <Button onClick={handleSubmit} className="w-full py-7">
+            Submit
+        </Button>
+    </div>
+  )
+}
+
+export default TopupForm
--- src/page/wallet/Wallet.jsx
+++ src/page/wallet/Wallet.jsx
@@ -1,7 +1,11 @@
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Dialog, DialogContent, DialogHeader, DialogTitle, DialogTrigger } from '@/components/ui/dialog'
 import { ReloadIcon } from '@radix-ui/react-icons'
-import { CopyIcon, DollarSign, WalletIcon } from 'lucide-react'
+import { CopyIcon, DollarSign, DownloadIcon, ShuffleIcon, UploadIcon, WalletIcon } from 'lucide-react'
 import React from 'react'
+import TopupForm from './TopupForm'
+import WithdrawalForm from './WithdrawalForm'
+import TransferForm from './TransferForm'
 
 const Wallet = () => {
   return (
@@ -34,6 +38,61 @@ const Wallet = () => {
             <div className='flex items-center'>
               <DollarSign></DollarSign>
               <span className='text-2xl font-semibold'>20000</span>
+
+            </div>
+
+            <div className='flex gap-7 mt-5'>
+
+              <Dialog>
+                <DialogTrigger>
+                  <div className='h-24 w-24 hover:text-gray-400 cursor-pointer flex flex-col items-center justify-center rounded-md
+                  shadow-slate-800 shadow-md'>
+                    <UploadIcon></UploadIcon>
+                    <span className='text-sm mt-2'>Add Money</span>
+
+                  </div>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle className="text-center text-xl">Top up your wallet</DialogTitle>
+                  </DialogHeader>
+                  <TopupForm></TopupForm>
+                </DialogContent>
+              </Dialog>
+
+              <Dialog>
+                <DialogTrigger>
+                  <div className='h-24 w-24 hover:text-gray-400 cursor-pointer flex flex-col items-center justify-center rounded-md
+                  shadow-slate-800 shadow-md'>
+                    <DownloadIcon></DownloadIcon>
+                    <span className='text-sm mt-2'>Withdrawal</span>
+
+                  </div>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle className="text-center text-xl">Request withdrawal</DialogTitle>
+                  </DialogHeader>
+                  <WithdrawalForm></WithdrawalForm>
+                </DialogContent>
+              </Dialog>
+
+              <Dialog>
+                <DialogTrigger>
+                  <div className='h-24 w-24 hover:text-gray-400 cursor-pointer flex flex-col items-center justify-center rounded-md
+                  shadow-slate-800 shadow-md'>
+                    <ShuffleIcon></ShuffleIcon>
+                    <span className='text-sm mt-2'>Transfer</span>
+
+                  </div>
+                </DialogTrigger>
+                <DialogContent>
+                  <DialogHeader>
+                    <DialogTitle className="text-center text-xl">Trasfer to other wallet</DialogTitle>
+                  </DialogHeader>
+                  <TransferForm></TransferForm>
+                </DialogContent>
+              </Dialog>
 
             </div>
           </CardContent>
